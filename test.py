f277ed08152eb649fd6c4bc0396fe04483e8594d
AnthonyP57 <antonipawlowicz123@gmail.com>
2025-09-09 18:49:27 +0200

    works

---
+++ test.py
@@ -0,0 +1,6 @@
+from fandom_scraper import scrape_fandom
+
+from fandom_scraper import scrape_fandom
+from pathlib import Path
+
+scrape_fandom(Path('./examples/witcher_json'), Path('./fandom'), Path('./fandom_instruct'))
